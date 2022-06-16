766cf6f623a4a249ce8f5de45f3fa8b96882a943
jay <jay.m.parmar2001@gmail.com>
2022-06-16 20:41:59 +0530

    tailwind setup

--- front-end-web/src/App.js
+++ front-end-web/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter ,Route,Routes } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Login from './pages/login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Login />} />
+        {/* <Route path='/users' exact element={<UserList />} />
+        <Route path='/edit/:id' exact element={<EditExercise />} />
+        <Route path='/create' exact element={<AddExercise />} />
+        <Route path='/users/create' exact element={<AddUser />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ front-end-web/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import './css/login.css';
+// import "https://cdn.tailwindcss.com";
+function Login(){
+    return(
+        <div className="grid grid-row-1">
+           <div className='grid grid-cols-12'>
+                <div className='grid col-span-10 col-start-2 box'>
+                        jay parmar
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default Login;
