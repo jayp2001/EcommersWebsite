7923c823ca1a0884de91b4803e3700cfe438b217
jay <jay.m.parmar2001@gmail.com>
2022-06-16 22:25:55 +0530

    login page completed

--- front-end-web/src/pages/login.jsx
+++ front-end-web/src/pages/login.jsx
@@ -15,10 +15,10 @@ function Login(){
                             </div>
                         </div>
                         <div className='card-right'>
-                            <div className='flex justify-center'>
-                                <div className='header'>
+                            <div className='flex justify-center mb-2 header'>
+                                {/* <div className='header'> */}
                                     Login
-                                </div>
+                                {/* </div> */}
                             </div>
                             <div className='input-fields-wrapper'>
                                 <div className='field-wrapper flex justify-center'>
@@ -26,6 +26,10 @@ function Login(){
                                 </div>
                                 <div className='field-wrapper flex justify-center'>
                                     <TextField id="standard-basic" InputProps={{ style: { fontSize: 22 } }} className='field' label="Password" variant="outlined" />
+                                </div>
+                                <div className='flex justify-between mt-12'>
+                                    <div><button className='signIn-btn'>Sign In</button></div>
+                                    <div><button className='forgot-btn'>Forgot Password</button></div>
                                 </div>
                             </div>
                         </div>
