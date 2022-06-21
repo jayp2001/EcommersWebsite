01c7a628c993691340a4d51070615b1864c72f98
jay <jay.m.parmar2001@gmail.com>
2022-06-21 14:07:09 +0530

    backend

---
+++ back-end/routes/user.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router();
+let user = require('../model/user.model');
+
+const {loginUser,registerUser} = require('../controller/userController')
+
+router.post('/',loginUser);
+router.post('/register', registerUser)
+// router.route('/').get((req,res) => {
+//     user.find()
+//     .then(users => res.json(users))
+//     .catch(err => res.status(400).json('Error : ' + err));
+// });
+
+// router.route('/add').post((req,res)=>{
+//     const userName = req.body.userName;
+
+//     const newUser = new user({userName});
+
+//     newUser.save()
+//         .then(()=> res.json("user added .....!"))
+//         .catch(err => res.status(400).json('Error : ' + err));
+// });
+
+module.exports = router;
--- front-end-web/src/pages/login/login.jsx
+++ front-end-web/src/pages/login/login.jsx
@@ -1,11 +1,40 @@
 import './css/login.css';
+import { useState } from 'react'
 import TextField from '@mui/material/TextField';
+import axios from 'axios'
+import * as constatnt from '../../constatnt/auth';
 import { useNavigate } from 'react-router-dom';
 function Login(){
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+      })
+      const onChange = (e) => {
+        setFormData((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+        }))
+      }
     let navigate = useNavigate();
     const forgotPwd = ()=>{
         navigate('/forgotPwd')
     }
+
+    const submitLogin = async (e)=>{
+       
+        e.preventDefault();
+        const res = await axios.post(`${constatnt.DB_URL}login/`,formData);
+
+        if(res.status === 400 || !res.data){
+            console.log(">>>>>>>",res.data)
+            alert("invalid user");
+        }
+        else{
+            console.log(">>>>>>>",res.data)
+            // navigate('/deskBoard')
+        }
+    }
+
     return(
         <>
         <div className='card-right'>
@@ -16,13 +45,13 @@ function Login(){
             </div>
             <div className='input-fields-wrapper'>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" InputProps={{ style: { fontSize: 22 } }} className='field' label="User Name" variant="outlined" />
+                    <TextField name='email' value={formData.email} onChange={onChange} InputProps={{ style: { fontSize: 22 } }} className='field' label="User Name" variant="outlined" />
                 </div>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" InputProps={{ style: { fontSize: 22 } }} className='field' label="Password" variant="outlined" />
+                    <TextField name='password' value={formData.password} onChange={onChange}  InputProps={{ style: { fontSize: 22 } }} className='field' label="Password" variant="outlined" />
                 </div>
                 <div className='flex justify-between mt-12'>
-                    <div><button className='signIn-btn'>Sign In</button></div>
+                    <div><button className='signIn-btn' onClick={submitLogin}>Sign In</button></div>
                     <div><button className='forgot-btn' onClick={()=>forgotPwd()}>Forgot Password</button></div>
                 </div>
             </div>
