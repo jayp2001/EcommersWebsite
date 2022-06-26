d12bd825e05c4af292528515840f0012c57e3744
Vikalp chavda <vikalp@vikalps-MacBook-Air.local>
2022-06-26 23:15:09 +0530

    signup

--- front-end-web/src/pages/login/signUp.jsx
+++ front-end-web/src/pages/login/signUp.jsx
@@ -2,12 +2,40 @@ import './css/login.css';
 import './css/signUp.css';
 import TextField from '@mui/material/TextField';
 import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import * as constatnt from '../../constatnt/auth';
 
 function SignUp() {
     let navigate = useNavigate();
     const cancle = ()=>{
         navigate('/');
     }
+
+    const [formdata,setFormdata] = useState({
+        userName :'',
+        email : '',
+        password : '',
+        address : '',
+    })
+
+    const onchange = (e) => {
+        setFormdata((prevState) => ({
+            ...prevState,
+          [e.target.name]: e.target.value,
+        }))
+    }
+    const submitAddUser = async (e)=>{
+       
+        e.preventDefault();
+        console.log(">>>>>",formdata)
+        const res = await axios.post(`${constatnt.DB_URL}login/register`,formdata)
+        .then((res)=>{
+            navigate("/")
+        })
+
+    }
+
     return(
         <>
         <div className='card-right'>
@@ -16,25 +44,25 @@ function SignUp() {
             </div>
             <div className='input-fields-wrapper'>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" className='field' label="User Name" variant="outlined" />
+                    <TextField id="standard-basic" className='field' onChange={onchange} name='userName' value={formdata.userName} label="User Name" variant="outlined" />
                 </div>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" className='field' label="Email" variant="outlined" />
+                    <TextField id="standard-basic" className='field' onChange={onchange} name='email' value={formdata.email} label="Email" variant="outlined" />
                 </div>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" className='field' label="Password" variant="outlined" />
+                    <TextField id="standard-basic" className='field' onChange={onchange} name='password' value={formdata.password} label="Password" variant="outlined" />
                 </div>
                 <div className='field-wrapper flex justify-center'>
                     <TextField id="standard-basic" className='field' label="Retype Password" variant="outlined" />
                 </div>
                 <div className='field-wrapper flex justify-center'>
-                    <TextField id="standard-basic" className='field' label="Address" variant="outlined" multiline rows={2} />
+                    <TextField id="standard-basic" className='field' onChange={onchange} name='address' value={formdata.assress} label="Address" variant="outlined" multiline rows={2} />
                 </div>
                 {/* <div className='field-wrapper flex justify-center'>
                     <TextField id="standard-basic" InputProps={{ style: { fontSize: 22 } }} className='field' label="Password" variant="outlined" />
                 </div> */}
                 <div className='flex justify-between mt-12'>
-                    <div><button className='signIn-btn'>SignUp</button></div>
+                    <div><button className='signIn-btn' onClick={submitAddUser}>SignUp</button></div>
                     <div><button className='forgot-btn' onClick={()=>cancle()}>Cancle</button></div>
                 </div>
             </div>
